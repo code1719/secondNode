6ae9fe80a2c8207720bb54ae53d3570cc178d7b2
code1719 <codyhearell@gmail.com>
2023-12-04 14:17:09 -0700

    added missing code

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const techMessage = require("../controllers/techController"); // Corrected import name
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
 router.get("/ttech", techMessage.ttechMessage); // Use techMessage instead of techController
 
 module.exports = router;
